2fc962ef1af084b55a8d830ce9f5994b1ed5d133
bambangpentolet <burhanandikaa@gmail.com>
2020-12-23 22:58:44 +0700

    edit koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,8 @@
 <?php
-// $host = "127.0.0.1"; // Nama hostnya
-// $username = "root"; // Username
-// $password = ""; // Password (Isi jika menggunakan password)
-// $database = "user"; // Nama databasenya
-// // Koneksi ke MySQL dengan PDO
-// $pdo = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
-mysql_connect("localhost","root",""); //sesuaikan dengan password dan username mysql anda
-mysql_select_db("user"); //nama database yang kita gunakan
+$host = "127.0.0.1"; // Nama hostnya
+$username = "root"; // Username
+$password = ""; // Password (Isi jika menggunakan password)
+$database = "user"; // Nama databasenya
+// Koneksi ke MySQL dengan PDO
+$pdo = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
 ?>
