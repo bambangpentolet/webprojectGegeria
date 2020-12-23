d160c7d7c37efb708078b2ea57868c828cfa1522
bambangpentolet <burhanandikaa@gmail.com>
2020-12-23 23:43:43 +0700

    edit koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,5 +4,5 @@ $username = "root"; // Username
 $password = ""; // Password (Isi jika menggunakan password)
 $database = "user"; // Nama databasenya
 // Koneksi ke MySQL dengan PDO
-$pdo = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
+$pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
 ?>
