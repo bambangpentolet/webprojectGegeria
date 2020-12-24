55aa20eac48f0b0991d6efa7ec141f86cbfb4c49
bambangpentolet <burhanandikaa@gmail.com>
2020-12-24 11:56:43 +0700

    edit login.php

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if( ! empty($data)){ // Jika tidak sama dengan empty (kosong)
   header("location: welcome.php"); // Kita redirect ke halaman welcome.php
 }else{ // Jika $data nya kosong
   // Buat sebuah cookie untuk menampung data pesan kesalahan
-  setcookie("message", "Maaf, Username atau Password salah", time()+3600);
+  setcookie("message", "Maaf, Username dan Password tidak boleh kosong dan salah", time()+3600);
   
   header("location: index.php"); // Redirect kembali ke halaman index.php
 }
